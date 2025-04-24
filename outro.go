29eee8244b6c901fd49f8996e85f9bc611d305bb
Gobetti <lucas.pucci@aluno.senai.br>
2025-04-24 14:18:35 -0300

    exercicioDois

---
+++ outro.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	capitais := map[string]string{
+		"SP" : "São Paulo",
+		"RJ" : "Rio de Janeiro",
+		"ES" : "Espírito Santo",
+	}
+
+	capitais["BH"] = "Belo Horizonte"
+
+	for k,v := range capitais {
+		fmt.Println("Sigla, Nome", k, v)
+	}
+
+	delete(capitais, "RJ")
+
+	for k,v := range capitais {
+		fmt.Println("Sigla, Nome", k, v)
+	}
+
+}
